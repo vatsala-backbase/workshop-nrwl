bcc41ecc076af584b1c424f6262974cc41abebf3
Vatsala <vatsala@backbase.com>
2021-12-13 17:55:11 +0000

    lab 4

--- bg-hoard/apps/bg-game/src/app/app.module.ts
+++ bg-hoard/apps/bg-game/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { RouterModule } from '@angular/router';
 import { MatCardModule } from '@angular/material/card';
+import { StoreUiSharedModule} from "@bg-hoard/store/ui-shared";
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
   imports: [
     BrowserModule,
     MatCardModule,
+    StoreUiSharedModule,
     RouterModule.forRoot([], { initialNavigation: 'enabledBlocking' }),
   ],
   providers: [],
