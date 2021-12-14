b01015ecc705bab8c25211b8ca4c16cec1146f62
Vatsala <vatsala@backbase.com>
2021-12-14 11:32:06 +0000

    lab 9

--- bg-hoard/apps/store/src/app/app.component.ts
+++ bg-hoard/apps/store/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
-import { getAllGames } from '../fake-api';
 import { formatRating } from '@bg-hoard/store/util-formatters';
 import { HttpClient } from '@angular/common/http';
+import { Game } from '@bg-hoard/util-interface';
 
 @Component({
   selector: 'bg-hoard-root',
@@ -11,6 +11,6 @@ import { HttpClient } from '@angular/common/http';
 export class AppComponent {
   formatRating = formatRating;
   title = 'Board Game Hoard';
-  games = this.http.get<any[]>('/api/games');
+  games = this.http.get<Game[]>('/api/games');
   constructor(private http: HttpClient) {}
 }
